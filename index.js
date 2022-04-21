1016987d813405c841ab1e41ed8ee110e2b46335
Eric <eric.yanez50@qmail.cuny.edu>
2022-04-21 01:04:14 -0400

    commit tree 5bd5b8d1e2bb7de9589791a0554ccdfac2f56672

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const names  = ["Guadalupe", "Ollie", "Aki"]
+const events = 'surprise'
+
+function writeCards(names){
+    let newArray = []
+    for(let i = 0; i < names.length; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`)
+    }
+    return newArray
+    
+}
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+countDown(4);
+
+
